2cc5e5d8d32e7af860dce2d8a1112f66c095e7e8
KEINOS <github@keinos.com>
2021-10-24 00:45:31 +0900

    docs: add comment for VerifySUM() (golint fix)

--- util/VerifySUM.go
+++ util/VerifySUM.go
@@ -2,6 +2,8 @@ package util
 
 import "fmt"
 
+// VerifySUM returns true if the sum is a valid checksum of the input with the given mask.
+// The sum value should be created via SUM() function with the same mask value.
 func VerifySUM(mask uint, input string, sum uint) bool {
 	filter := GenMask(len(fmt.Sprintf("%b", mask))) // create N digit filter
 
